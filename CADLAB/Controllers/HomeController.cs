256bfa89ff584a934d835d09403ca0b111df750f
gis <gis@VMCADLAB>
2016-09-05 14:09:25 +0000

    Add project files.

---
+++ CADLAB/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.ApplicationInsights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CADLAB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Crash()
+        {
+            throw new Exception("Boooom!");
+        }
+
+        public ActionResult LongOperation()
+        {
+            Thread.Sleep(10000);
+
+            return View();
+        }
+
+        public ActionResult CustomEvents()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CustomEvents(string eventName)
+        {
+            var telemetry = new TelemetryClient();
+
+            telemetry.TrackEvent(eventName);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CADLAB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CADLAB.Startup))]
+namespace CADLAB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
